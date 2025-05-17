9b7139174639f5f85de8f0e5bd21b6d4864acc71
sectepOfficial <faraonmakster@gmail.com>
2025-05-17 17:00:00 +0300

    Add files via upload
    reuploaded main class  of mistaken upload before

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
-package personalFinanceManager.src;
+package BloodMatch.src.bloodmatch;
 
+// a class, where the functionality of the project is manages.
 public class Main {
     public static void main(String[] args) {
+
         ConsoleInterface app = new ConsoleInterface();
-        app.launchApp();
+
+        app.launch();
     }
 }
